0e5224bf01c0bf15f3f58096beafd758ef55ed30
Florent Cramette <fce@easi-soft.fr>
2025-04-03 14:52:38 +0200

    creation module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate",
+    'summary': "Gestion des annonces immobilières.",
+    'description': """
+        Ce module permet de gérer des annonces immobilières.
+        Pour l'instant, il s'agit d'une coquille vide qui sera enrichie dans les prochains chapitres.
+    """,
+    'author': "Votre Nom",
+    'website': "http://www.votresite.com",
+    'category': 'Real Estate',
+    'version': '0.1',
+    'depends': ['base'],  # Le seul module framework nécessaire pour l'instant.
+    'application': True,  # Pour que le module apparaisse dans le filtre « Applications »
+}
